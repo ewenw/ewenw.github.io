ebe8fc7bcedbe2a1bf4812770da57982071db944
superyuxuan <superyuxuan@gmail.com>
2018-04-29 23:53:17 -0400

    overhaul

--- index_script.js
+++ index_script.js
@@ -1,8 +1,3 @@
-/*d3.json("navigation.json", function(error, data) {
-    console.log(data); // this is your data
-});
-*/
-
 const width = window.innerWidth * 0.75;
 const height = window.innerHeight * 0.75;
 
@@ -12,12 +7,14 @@ const nodes = [
   { id: "games", link: "", label: "Games", level: 2 },
   { id: "software", link: "", label: "Software", level: 2 },
   { id: "research", link: "", label: "Research", level: 2 },
-  { id: "optimal", link: "https://github.com/ewenw/OptimalDesign", label: "Optimal Design", level: 3, description: "this is a description"},
-  { id: "rocket", link: "https://play.google.com/store/apps/details?id=com.ween.rocket&hl=en_US", label: "Land the Rocket", level: 3 , description: "this is a description"},
-  { id: "evorite", link: "https://play.google.com/store/apps/details?id=com.complex51.evorite", label: "Evorite", level: 3 , description: "this is a description"},
-  { id: "swipe", link: "https://play.google.com/store/apps/details?id=com.weengames.smashballistics", label: "Swipe Ballistics", level: 3 , description: "this is a description"},
+  { id: "optimal", link: "https://github.com/ewenw/OptimalDesign", label: "Optimal Design", level: 3, description: "Selecting the best experiments through simulation models and a GP search algorithm"},
+  { id: "rocket", link: "https://play.google.com/store/apps/details?id=com.ween.rocket&hl=en_US", label: "Land the Rocket", level: 3 , description: "Experience the thrill of landing a reusable orbital booster on a drone ship in the middle of the ocean."},
+  { id: "evorite", link: "https://play.google.com/store/apps/details?id=com.complex51.evorite", label: "Evorite", level: 3 , description: "Explore the mysteries of the solar system through breathtaking graphics"},
+  { id: "swipe", link: "https://play.google.com/store/apps/details?id=com.weengames.smashballistics", label: "Swipe Ballistics", level: 3 , description: "Master ballistic physics and throw projectiles to get through obstacles"},
   { id: "prisoner", link: "https://github.com/ewenw/prisonersgame", label: "Prisoner's Dilemma", level: 4, description: "Prisoner's dilemma implmementation for running live web experiments"},
-  { id: "actions", link: "https://github.com/ewenw/actions", label: "Actions", level: 4, description: "Live macro keyboard and mouse recorder in Java"}
+  { id: "actions", link: "https://github.com/ewenw/actions", label: "Actions", level: 4, description: "Live macro keyboard and mouse recorder in Java"},
+  { id: "hackdartmouth", link: "https://github.com/ewenw/YelpMyProfessors", label: "YelpMyProfessors", level: 4, description: "Tired of reading reviews? Just paste them in for an overall rating!"},
+  { id: "nodegame", link: "https://nodegame.org", label: "nodeGame", level: 4, description: "Fast, scalable JavaScript for large-scale, online, multiplayer, real-time games and experiments"}
   
 ];
 
@@ -28,13 +25,20 @@ const links = [
   { target: "games", source: "swipe" , strength: 1 },
   { target: "software", source: "portfolio" , strength: 1.5 },
   { target: "software", source: "actions" , strength: 1 },
+  { target: "software", source: "actions" , strength: 1 },
   { target: "research", source: "portfolio" , strength: 1.5 },
   { target: "research", source: "optimal" , strength: 1.2 },
   { target: "optimal", source: "prisoner" , strength: 1.2 },
+  { target: "nodegame", source: "prisoner" , strength: 1.2 },
+  { target: "nodegame", source: "research" , strength: 1.2 },
+  { target: "hackdartmouth", source: "software" , strength: 1.2 },
+  { target: "evorite", source: "rocket" , strength: 1.2 },
+  { target: "rocket", source: "swipe" , strength: 1.2 },
+  
 ];
 
 const simulation = d3.forceSimulation(nodes)
-  .velocityDecay(0.8)
+  .velocityDecay(0.92)
   .force('charge', d3.forceManyBody().strength(-4000)) 
   .force('center', d3.forceCenter(width / 2, height / 2))
   .force('link', d3.forceLink(links)
@@ -47,12 +51,7 @@ const simulation = d3.forceSimulation(nodes)
 const svg = d3.select('svg')
   .attr('width', width)
   .attr('height', height);
-
-// adds connected nodes for decoration
-function addDecorationNodes() {
-
-}
-
+  
 function getNodeColor(node) {
     const levelColors = ["white", "cyan", "teal", "orange"];
     return levelColors[node.level-1];
@@ -138,7 +137,6 @@ function mouseOut(node) {
     d3.select(this).transition()
       .duration(200)
       .attr("r", getNodeRadius(node));
-    d3.selectAll(".description").html("");
 }
 
 nodeGraphics
