5370bee36e1209fef1d90bec6b52090ff71d6a7a
superyuxuan <superyuxuan@gmail.com>
2018-07-13 00:54:10 -0400

    updates

--- index_script.js
+++ index_script.js
@@ -3,18 +3,19 @@ const height = window.innerHeight * 0.75;
 
      
 const nodes = [ 
-  { id: "portfolio", link: "", label: "Projects", level: 1 },
+  { id: "portfolio", link: "", label: "", level: 1 },
   { id: "games", link: "", label: "Games", level: 2 },
   { id: "software", link: "", label: "Software", level: 2 },
   { id: "research", link: "", label: "Research", level: 2 },
   { id: "optimal", link: "https://github.com/ewenw/OptimalDesign", label: "Optimal Design", level: 3, description: "Selecting the best experiments through simulation models and a GP search algorithm"},
-  { id: "rocket", link: "https://play.google.com/store/apps/details?id=com.ween.rocket&hl=en_US", label: "Land the Rocket", level: 3 , description: "Experience the thrill of landing a reusable orbital booster on a drone ship in the middle of the ocean."},
-  { id: "evorite", link: "https://play.google.com/store/apps/details?id=com.complex51.evorite", label: "Evorite", level: 3 , description: "Explore the mysteries of the solar system through breathtaking graphics"},
-  { id: "swipe", link: "https://play.google.com/store/apps/details?id=com.weengames.smashballistics", label: "Swipe Ballistics", level: 3 , description: "Master ballistic physics and throw projectiles to get through obstacles"},
-  { id: "prisoner", link: "https://github.com/ewenw/prisonersgame", label: "Prisoner's Dilemma", level: 4, description: "Prisoner's dilemma implmementation for running live web experiments"},
-  { id: "actions", link: "https://github.com/ewenw/actions", label: "Actions", level: 4, description: "Live macro keyboard and mouse recorder in Java"},
-  { id: "hackdartmouth", link: "https://github.com/ewenw/YelpMyProfessors", label: "YelpMyProfessors", level: 4, description: "Tired of reading reviews? Just paste them in for an overall rating!"},
-  { id: "nodegame", link: "https://nodegame.org", label: "nodeGame", level: 4, description: "Fast, scalable JavaScript for large-scale, online, multiplayer, real-time games and experiments"}
+  { id: "rocket", link: "https://play.google.com/store/apps/details?id=com.ween.rocket&hl=en_US", label: "Land the Rocket", level:4 , description: "Experience the thrill of landing a reusable orbital booster on a drone ship in the middle of the ocean."},
+  { id: "evorite", link: "https://play.google.com/store/apps/details?id=com.complex51.evorite", label: "Evorite", level: 4 , description: "Explore the mysteries of the solar system through breathtaking graphics"},
+  { id: "swipe", link: "https://play.google.com/store/apps/details?id=com.weengames.smashballistics", label: "Swipe Ballistics", level: 4 , description: "Master ballistic physics and throw projectiles to get through obstacles"},
+  { id: "prisoner", link: "https://github.com/ewenw/prisonersgame", label: "Prisoner's Dilemma", level:3, description: "Prisoner's dilemma implmementation for running live web experiments"},
+  { id: "actions", link: "https://github.com/ewenw/actions", label: "Actions", level: 5, description: "Live macro keyboard and mouse recorder in Java"},
+  { id: "hackdartmouth", link: "https://github.com/ewenw/YelpMyProfessors", label: "YelpMyProfessors", level: 5, description: "Tired of reading reviews? Just paste them in for an overall rating!"},
+  { id: "nodegame", link: "https://nodegame.org", label: "nodeGame", level: 3, description: "Fast, scalable JavaScript for large-scale, online, multiplayer, real-time games and experiments"},
+  { id: "hawkdove", link: "https://github.com/ewenw/HawkDove", label: "Hawk-Dove", level: 5, description: "Large-scale experiment for the study of innovation in dynamic networks"}
   
 ];
 
@@ -25,7 +26,7 @@ const links = [
   { target: "games", source: "swipe" , strength: 1 },
   { target: "software", source: "portfolio" , strength: 1.5 },
   { target: "software", source: "actions" , strength: 1 },
-  { target: "software", source: "actions" , strength: 1 },
+  { target: "software", source: "hawkdove" , strength: 1.5 },
   { target: "research", source: "portfolio" , strength: 1.5 },
   { target: "research", source: "optimal" , strength: 1.2 },
   { target: "optimal", source: "prisoner" , strength: 1.2 },
@@ -53,17 +54,17 @@ const svg = d3.select('svg')
   .attr('height', height);
   
 function getNodeColor(node) {
-    const levelColors = ["white", "cyan", "teal", "orange"];
+    const levelColors = ["white", "cyan", "purple", "orange", "teal", "green"];
     return levelColors[node.level-1];
 }
 
 function getNodeRadius(node) {
-    const radii = [10, 5, 24, 18];
+    const radii = [10, 5, 24, 18, 20, 15];
     return radii[node.level-1];
 }
 
 function getFontSize(node) {
-    const font = [14, 15, 16, 14];
+    const font = [14, 15, 16, 14, 14, 14, 14];
     return font[node.level-1];
 }
 
