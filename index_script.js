f806472a3839b47df35ff6398179babfe89b1e77
ewenw <wang.yuxu@northeastern.edu>
2019-05-04 20:41:14 -0400

    new look

--- index_script.js
+++ index_script.js
@@ -1,54 +1,55 @@
-const width = window.innerWidth * 0.75;
-const height = window.innerHeight * 0.65;
+const width = 1000;
+const height = 900;
 
      
 const nodes = [ 
-  { id: "portfolio", link: "https://github.com/ewenw/", description: "Projects", label: "", level: 1 },
-  { id: "games", link: "", label: "Games", level: 2 },
-  { id: "software", link: "", label: "Software", level: 2 },
-  { id: "research", link: "", label: "Research", level: 2 },
-  { id: "optimal", link: "OptimalDesign.pdf", label: "Optimal Design", level: 3, description: "Information theory: selecting the optimal parameters for experiments through simulations and a GP search algorithm"},
-  { id: "rocket", link: "https://play.google.com/store/apps/details?id=com.ween.rocket&hl=en_US", label: "Land the Rocket", level:4 , description: "Try to land a reusable orbital booster on a drone ship in the middle of the ocean. Inspired by SpaceX's Falcon 9"},
-  { id: "evorite", link: "https://play.google.com/store/apps/details?id=com.complex51.evorite", label: "Evorite", level: 4 , description: "Explore the solar system's mysteries through the thrilling adventure of a space probe"},
-  { id: "swipe", link: "https://play.google.com/store/apps/details?id=com.weengames.smashballistics", label: "Swipe Ballistics", level: 4 , description: "Use physics to your advantage. Fire projectiles to get through obstacles and defend yourself"},
-  { id: "prisoner", link: "https://github.com/ewenw/prisonersgame", label: "Prisoner's Dilemma", level:3, description: "Prisoner's dilemma implmementation for running live web experiments"},
-  { id: "actions", link: "https://github.com/ewenw/actions", label: "Actions", level: 5, description: "Live macro recording software in Java"},
-  { id: "hackdartmouth", link: "https://github.com/ewenw/YelpMyProfessors", label: "YelpMyProfessors", level: 5, description: "Sentiment analyzer that predicts ratings for comments and reviews"},
-  { id: "nodegame", link: "https://nodegame.org", label: "nodeGame", level: 3, description: "Fast, scalable JavaScript for large-scale, online, multiplayer, real-time games and experiments"},
-  { id: "hawkdove", link: "https://github.com/ewenw/HawkDove", label: "HawkDove", level: 5, description: "Behavior science multiplayer web experiment to study the role of innovation in dynamic networks"},
-  { id: "scalacask", link: "https://github.com/ewenw/ScalaCask", label: "ScalaCask", level: 5, description: "Lightweight, high throughput key-value store"}
+  { id: "games", link: "", label: "Games", level: 1 },
+  { id: "software", link: "", label: "Software", level: 1 },
+  { id: "research", link: "", label: "Research", level: 1 },
+  { id: "optimal", link: "OptimalDesign.pdf", label: "Optimal Design", level: 3, description: "Search algorithm for the optimal parameters of a simulated experiment"},
+  { id: "rocket", link: "https://play.google.com/store/apps/details?id=com.ween.rocket&hl=en_US", label: "Land the Rocket", level:4 , description: "Land a SpaceX orbital booster on a drone ship in the middle of the ocean"},
+  { id: "evorite", link: "https://play.google.com/store/apps/details?id=com.complex51.evorite", label: "Evorite", level: 2 , description: "Explore the solar system's mysteries through the adventure of a space probe"},
+  { id: "swipe", link: "https://play.google.com/store/apps/details?id=com.weengames.smashballistics", label: "Swipe Ballistics", level: 2 , description: "Smash through obstacles and defend yourself by tossing projectiles"},
+  { id: "prisoner", link: "https://github.com/ewenw/prisonersgame", label: "Prisoner's Dilemma", level: 3, description: "Prisoner's dilemma implementation for running live web experiments"},
+  { id: "actions", link: "https://github.com/ewenw/actions", label: "Actions", level: 3, description: "Live macro recording software in Java"},
+  { id: "hackdartmouth", link: "https://github.com/ewenw/YelpMyProfessors", label: "YelpMyProfessors", level: 3, description: "Sentiment analyzer that predicts ratings for comments and reviews"},
+  { id: "nodegame", link: "https://nodegame.org", label: "nodeGame", level: 3, description: "JavaScript framework for running real-time behavioral science experiments"},
+  { id: "hawkdove", link: "https://github.com/ewenw/HawkDove", label: "HawkDove", level: 3, description: "Behavior science multiplayer experiment to study the role of innovation in dynamic networks"},
+  { id: "scalacask", link: "https://github.com/ewenw/ScalaCask", label: "ScalaCask", level: 3, description: "Lightweight, high throughput key-value store based on BitCask"},
+  { id: "authorml", link: "https://github.com/ewenw/AuthorML", label: "AuthorML", level: 3, description: "Classifying the classics - authorship identification through machine learning"}
   
 ];
 
-const degreeOne = 1;
-const degreeTwo = 1.2;
+const degreeOne = 3;
+const degreeTwo = 1.9;
+const nodeHeight = 40;
 
 const links = [
-  { target: "games", source: "portfolio" , strength: degreeOne },
+  { target: "games", source: "software" , strength: degreeOne },
+  { target: "software", source: "research" , strength: degreeOne },
+  { target: "research", source: "games" , strength: degreeOne },
   { target: "games", source: "rocket" , strength: degreeTwo },
   { target: "games", source: "evorite" , strength: degreeTwo },
   { target: "games", source: "swipe" , strength: degreeTwo },
-  { target: "software", source: "portfolio" , strength: degreeOne },
   { target: "software", source: "actions" , strength: degreeTwo },
   { target: "software", source: "scalacask" , strength: degreeTwo },
   { target: "software", source: "hawkdove" , strength: degreeTwo },
-  { target: "research", source: "portfolio" , strength: degreeOne },
   { target: "research", source: "optimal" , strength: degreeTwo },
   { target: "research", source: "prisoner" , strength: degreeTwo },
-  { target: "nodegame", source: "research" , strength: degreeTwo },
+  { target: "research", source: "authorml" , strength: degreeTwo },
+  { target: "nodegame", source: "software" , strength: degreeTwo },
   { target: "hackdartmouth", source: "software" , strength: degreeTwo }
   
 ];
 
 const simulation = d3.forceSimulation(nodes)
-  .velocityDecay(0.92)
-  .force('charge', d3.forceManyBody().strength(-2800)) 
+  .velocityDecay(0.99)
+  .force('charge', d3.forceManyBody().strength(-10000)) 
   .force('center', d3.forceCenter(width / 2, height / 2))
   .force('link', d3.forceLink(links)
   .id(link => link.id)
   .strength(link => link.strength))
   .force('attract', attract()
-    .target([width / 2, height / 2])
     .strength(function (d) { return 0.1; }));
 
 const svg = d3.select('svg')
@@ -57,22 +58,23 @@ const svg = d3.select('svg')
 
 svg.style("opacity", 0.0)
   .transition()
-  .duration(1500)
+  .duration(2500)
   .style("opacity", 1.0)
 
 function getNodeColor(node) {
-    const levelColors = ["black", "cyan", "purple", "orange", "teal", "green"];
+    const levelColors = ["#31485b", "#31485b", "#31485b", "#31485b"];
     return levelColors[node.level-1];
 }
 
-function getNodeRadius(node) {
-    const radii = [10, 5, 18, 18, 18, 18, 18];
-    return radii[node.level-1];
+function getNodeSize(node) {
+  if (node.level == 1)
+    return 0;
+  return node.label.length * 3 + 55;
 }
 
 function getFontSize(node) {
-    const font = [14, 15, 16, 14, 14, 14, 14];
-    return font[node.level-1];
+    const font = [24, 18, 18, 18, 18];
+    return font[node.level]-node.label.length / 3;
 }
 
 const linkGraphics = svg.append('g')
@@ -80,20 +82,23 @@ const linkGraphics = svg.append('g')
     .selectAll("line")
     .data(links)
     .enter().append("line")
-      .attr("stroke-width", 1)
-	  .attr("stroke", "black");
+      .attr("stroke-width", 0.4)
+	    .attr("stroke", "white");
 
 const nodeGraphics = svg.append('g')
     .selectAll('a')
     .data(nodes)
     .enter()
         .append("a")
-        .attr("xlink:href", function(d){ return d.link === "" ? "#" : d.link; })
-        .attr('target', function(d){ return d.link === "" ? "" : "_blank'"; })
-        .append('circle')
-            .attr("class", "node")
-            .attr('r', getNodeRadius)
-            .attr('fill', getNodeColor);
+        .append("rect")
+        .attr("class", function(d) {return "node-"+d.id;})
+        .attr("x", -getNodeSize/2)
+        .attr("y", -nodeHeight/2)
+        .attr("width", getNodeSize)
+        .attr("height", nodeHeight)
+        .attr("rx", 50)
+        .attr("ry", 50)
+        .attr('fill', getNodeColor);
 
 
 const nodeLabels = svg.append('g')
@@ -104,16 +109,35 @@ const nodeLabels = svg.append('g')
     .attr('font-size', getFontSize)
     .attr('font-family', "'Trebuchet MS', Helvetica, sans-serif")
     .attr('text-anchor', 'middle')
-    .attr('fill', 'grey') 
-    .attr('class', 'text-label');
+    .attr('fill', 'white') 
+    .attr('class', function(d){return 'text-label-'+d.id});
+
+const nodeOverlay = svg.append('g')
+    .selectAll('a')
+    .data(nodes)
+    .enter()
+        .append("a")
+        .attr("xlink:href", function(d){ return d.link === "" ? "#" : d.link; })
+        .attr('target', function(d){ return d.link === "" ? "" : "_blank'"; })
+        .append("rect")
+        .attr("x", -getNodeSize/2)
+        .attr("y", -nodeHeight/2)
+        .attr("width", getNodeSize)
+        .attr("height", nodeHeight)
+        .attr("rx", 10)
+        .attr("ry", 10)
+        .attr('fill', 'transparent');
 
 simulation.nodes(nodes).on('tick', () => {
     nodeGraphics
-        .attr("cx", function(d) { return d.x = Math.max(20, Math.min(width - 20, d.x)); })
-        .attr("cy", function(d) { return d.y = Math.max(40, Math.min(height - 20, d.y)); })
+        .attr("x", function(d) { return d.x - getNodeSize(d)/2})
+        .attr("y", function(d) { return d.y - nodeHeight/2 -5}) 
+    nodeOverlay
+        .attr("x", function(d) { return d.x - getNodeSize(d)/2})
+        .attr("y", function(d) { return d.y - nodeHeight/2 -5})
     nodeLabels
         .attr("x", node => node.x)
-        .attr("y", node => node.y - 30);
+        .attr("y", node => node.y);
     linkGraphics
         .attr('x1', link => link.source.x)
         .attr('y1', link => link.source.y)
@@ -122,15 +146,20 @@ simulation.nodes(nodes).on('tick', () => {
 })
 
 function mouseOver(node) {
-    if(node.link !== "") {
-        node.fx = d3.mouse.x;
-        node.fy = d3.mouse.y;
-        d3.select(this).style("cursor", "pointer"); 
-        d3.select(this).transition()
+  if(node.link !== "") {
+      node.fx = d3.mouse.x;
+      node.fy = d3.mouse.y;
+      d3.select(this).style("cursor", "pointer"); 
+      d3.selectAll(".node-"+node.id).transition()
         .duration(100)
-        .attr("r", getNodeRadius(node) * 1.64);
-        d3.selectAll(".description").html(node.description);
-    }
+        .attr("rx", "10px")
+        .attr("ry", "10px")
+        .attr("fill", "white");
+      d3.selectAll("#description").html(node.description);
+      d3.selectAll(".text-label-"+node.id).transition()
+        .duration(100)
+        .attr("fill", "black");
+  }
 }
 
 svg.on('mousemove', function () {
@@ -142,17 +171,22 @@ svg.on('mousemove', function () {
   
 function mouseOut(node) {
     simulation.restart();
-    d3.select(this).transition()
+    d3.selectAll(".node-"+node.id).transition()
       .duration(400)
-      .attr("r", getNodeRadius(node));
+      .attr("rx", "50px")
+      .attr("ry", "50px")
+      .attr("fill", getNodeColor);
+    d3.selectAll(".text-label-"+node.id).transition()
+      .duration(400)
+      .attr("fill", "white");
 }
 
-nodeGraphics
+nodeOverlay
     .on('mouseover', mouseOver)
     .on('mouseout', mouseOut);
 
 svg.on('zoom', function () {
     this.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
-  });
+});
 
 
